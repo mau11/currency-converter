88eb58f0044bcfeefa5f8d80c61e652e4d6a0547
Maureen E. Mirville <maureen.mirville@gmail.com>
2020-07-08 16:44:36 -0400

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+
+  render() {
+    return (
+      <div>
+        <footer className="footer mt-10">
+          <div>Exchange rates provided by <a href="https://exchangeratesapi.io">Madis Väin's Exchange API.</a>
+          </div>
+          <small>App created by Maureen <img src="./images/smiley.svg" />
+            <div className="container">
+              <a href="https://www.linkedin.com/in/mau11/">
+                <img className="icons" alt="linkedin profile" src="./images/linkedin.svg" />
+              </a>
+              <a href="https://github.com/mau11/image-finder">
+                <img className="icons" alt="github source code" src="./images/github.png" />
+              </a>
+            </div>
+          </small>
+        </footer>
+      </div>
+    )
+  }
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Calculator from './Calculator.jsx'
+import Footer from './Footer.jsx'
+
 
 export default class Layout extends Component {
 
@@ -8,7 +10,10 @@ export default class Layout extends Component {
     return (
       <div className="container text-center mt-5">
         <h1 className="font-weight-light text-center">Currency Exchange</h1>
+        <hr />
         <Calculator />
+        <hr />
+        <Footer />
       </div>
     )
   }
