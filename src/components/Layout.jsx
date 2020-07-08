5d906ce1201ee89463f0f4edbff89a4f0741116e
Maureen E. Mirville <maureen.mirville@gmail.com>
2020-07-08 15:24:39 -0400

    Update styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Calculator from './Calculator.jsx'
 
 export default class Layout extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Currency Exchange</h1>
+      <div className="container text-center mt-5">
+        <h1 className="font-weight-light text-center">Currency Exchange</h1>
+        <Calculator />
       </div>
     )
   }
