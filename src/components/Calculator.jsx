aa77a293263eaae65bbf0067091392833fce26fd
Maureen E. Mirville <maureen.mirville@gmail.com>
2020-07-09 09:37:45 -0400

    Refactor exchange calculation and update styling

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,57 +7,32 @@ export default class Search extends Component {
       api_url: 'https://api.exchangeratesapi.io/latest?base=',
       rates: '',
       amount: 0,
-      baseCurrency: '',
       base: [],
-      secondaryCurrency: '',
       secondary: [],
-      warning: true,
       iso_codes: [
-        '','AUD','BGN','BRL','CAD','CHF','CNY','CZK','DKK','EUR','GBP','HKD','HUF','IDR','ILS','INR','ISK','JPY','KRW','MXN','MYR','NOK','NZD','PHP','PLN','RON','RUB','SEK','SGD','THB','TRY','USD'
+        'AUD','BGN','BRL','CAD','CHF','CNY','CZK','DKK','EUR','GBP','HKD','HUF','IDR','ILS','INR','ISK','JPY','KRW','MXN','MYR','NOK','NZD','PHP','PLN','RON','RUB','SEK','SGD','THB','TRY','USD'
       ]
     }
-    this.handleBase = this.handleBase.bind(this)
-    this.handleSecondary = this.handleSecondary.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  getRates(type) {
-    if (this.state.baseCurrency !== '' && this.state.secondaryCurrency !== '') {
-      this.setState({warning: false})
-    }
-    let base = this.state.baseCurrency
-    if (base.length) {
-      return fetch(this.state.api_url+base)
-      .then(res => res.json())
-      .then(results => {
-        this.setState({[type]: results})
-      })
-      .catch(err => {console.log('ERROR', err)})
-    }
-  }
-
-  handleBase(e) {
-    if (e.target.value.length) {
-      this.setState({baseCurrency: e.target.value}, () => {
-        this.getRates('base')
-      })
-    }
-  }
-
-  handleSecondary(e) {
-    if (e.target.value.length) {
-      this.setState({secondaryCurrency: e.target.value}, () => {
-        this.getRates('secondary')
-      })
-    }
+  getRates(iso, secondary, amount) {
+    fetch(this.state.api_url+iso)
+    .then(res => res.json())
+    .then(results => {
+      let result = (amount * results.rates[secondary]).toFixed(2)
+      this.setState({result: result})
+    })
+    .catch(err => {console.log('ERROR', err)})
   }
 
   handleSubmit(e){
     e.preventDefault()
     const data = new FormData(e.target)
     let amount = data.get('amount')
-    let result = amount * this.state.base.rates[this.state.secondaryCurrency]
-    this.setState({amount: amount, result: result})
+    let baseIso = data.get('base')
+    let secondaryIso = data.get('secondary')
+    this.getRates(baseIso, secondaryIso, amount)
   }
 
   render() {
@@ -65,33 +40,26 @@ export default class Search extends Component {
       <div className="container col-6 col-offset-6">
         <form className="" onSubmit={this.handleSubmit}>
           <div className="form-group row m-3">
-            <input className="form-control col-6" id="amount" name="amount" placeholder="Enter amount"/>
-            <select className="form-control col-6" onChange={this.handleBase}>
+            <input className="form-control col-6" id="amount" name="amount" placeholder="Enter amount" required/>
+            <select className="form-control col-6" defaultValue="USD" name="base">
               {this.state.iso_codes.map((code, i) => (
-                <option id={'iso-'+i} key={i}>
-                {code}
-                </option>
+                <option id={'iso-'+i} key={i}>{code}</option>
               ))}
             </select>
           </div>
           <div className="form-group row m-3">
-            <input className="form-control col-6" readOnly id="secondary-currency" placeholder={this.state.result}/>
-            <select className="form-control col-6" onChange={this.handleSecondary}>
+            <div className="form-control col-6" readOnly id="secondary-currency" placeholder="">{this.state.result}
+            </div>
+            <select className="form-control col-6" defaultValue="EUR" name="secondary">
               {this.state.iso_codes.map((code, i) => (
-              <option id={'iso-'+i} key={i}>
-              {code}
-              </option>
+              <option id={'iso-'+i} key={i}>{code}</option>
             ))}
             </select>
           </div>
-          {this.state.warning ?
-          <small className="warning">Please select both exchange currencies.</small>
-          :
           <div className="col-12">
             <button className="btn btn-secondary">Get Rates
             </button>
           </div>
-           }
         </form>
       </div>
     )
